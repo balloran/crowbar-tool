18a7871274ea8ea26aca81997e99370989149ca7
Eduard Kamburjan <eduard@ifi.uio.no>
2021-10-11 14:13:08 +0200

    Fixed minor bug in exception testing

--- src/test/kotlin/ExceptionTest.kt
+++ src/test/kotlin/ExceptionTest.kt
@@ -6,17 +6,22 @@ import java.nio.file.Paths
 
 class ExceptionTest : StringSpec({
     val postInv = PostInvType::class
+    val cvc: String = System.getenv("CVC") ?: "cvc"
+    val z3: String = System.getenv("Z3") ?: "z3"
 
-        "basic exception"{
-            smtPath = System.getenv("Z3") ?: "z3"
+    for (smt in listOf(z3, cvc)) {
+        println("testing with: $smt as backend")
+        "$smt basic exception"{
+            smtPath = smt
             val (model, repos) = load(listOf(Paths.get("src/test/resources/except.abs")))
             val classDecl = model.extractClassDecl("M", "C")
 
             var res = classDecl.extractMethodNode(postInv, "success1", repos)
             executeNode(res, repos, postInv) shouldBe true
-            for(i in 1..3) {
+            for (i in 1..3) {
                 res = classDecl.extractMethodNode(postInv, "fail$i", repos)
                 executeNode(res, repos, postInv) shouldBe false
             }
         }
+    }
 })
